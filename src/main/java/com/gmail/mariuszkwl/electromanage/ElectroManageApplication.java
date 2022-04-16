8f1b7b7c84a1b2e48167f68c505a554818c70195
Mariusz Kowalczyk <mariuszkwl@gmail.com>
2022-04-16 20:28:09 +0200

    initial commit

---
+++ src/main/java/com/gmail/mariuszkwl/electromanage/ElectroManageApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.mariuszkwl.electromanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectroManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectroManageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmail/mariuszkwl/electromanage/ElectroManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.mariuszkwl.electromanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectroManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
