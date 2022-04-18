059c795c48cf9c4fb236c20e2aca0b1295493cdf
Mariusz Kowalczyk <mariuszkwl@gmail.com>
2022-04-18 16:39:01 +0200

    created ProductRepository

---
+++ src/main/java/com/gmail/mariuszkwl/electromanage/ProductRepository.java
@@ -0,0 +1,6 @@
+package com.gmail.mariuszkwl.electromanage;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+}
