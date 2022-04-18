67dc7eef1da6638aea11f8e1326e992b23b0a9e4
Mariusz Kowalczyk <mariuszkwl@gmail.com>
2022-04-18 11:12:49 +0200

    created ClientRepository

---
+++ src/main/java/com/gmail/mariuszkwl/electromanage/ClientRepository.java
@@ -0,0 +1,6 @@
+package com.gmail.mariuszkwl.electromanage;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClientRepository extends CrudRepository<Client, Integer> {
+}
